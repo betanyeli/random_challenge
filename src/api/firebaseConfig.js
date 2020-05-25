b32e2c7ec5e0453924532b4fceb01971ae3795a7
betanyeli <betangelii@gmail.com>
2020-05-25 19:29:02 -0400

    adding firebase config & hiding aws enviroments variables

---
+++ src/api/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const  firebaseConfig = {
+    apiKey: "AIzaSyBwtQc2fPcoD9sFSARZn_h5fW6JTIin4b8",
+    authDomain: "random-users-challenge.firebaseapp.com",
+    databaseURL: "https://random-users-challenge.firebaseio.com",
+    projectId: "random-users-challenge",
+    storageBucket: "random-users-challenge.appspot.com",
+    messagingSenderId: "696399636802",
+    appId: "1:696399636802:web:55537a52e18102bc66c0db"
+  };

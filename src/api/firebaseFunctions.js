b63a57a83b9d63700abbab49b9b4311f7c8e8de3
betanyeli <betangelii@gmail.com>
2020-05-25 21:43:09 -0400

    Login with google it's working

--- src/api/firebaseFunctions.js
+++ src/api/firebaseFunctions.js
@@ -1,23 +1,8 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from '../api/firebaseConfig';
 
-const  firebaseConfig = {
-    apiKey: "AIzaSyBwtQc2fPcoD9sFSARZn_h5fW6JTIin4b8",
-    authDomain: "random-users-challenge.firebaseapp.com",
-    databaseURL: "https://random-users-challenge.firebaseio.com",
-    projectId: "random-users-challenge",
-    storageBucket: "random-users-challenge.appspot.com",
-    messagingSenderId: "696399636802",
-    appId: "1:696399636802:web:55537a52e18102bc66c0db"
-  };
-
-  firebase.initializeApp(firebaseConfig);
-  //console.log("firebaseinit",firebase.initializeApp(firebaseConfig) )
-  const provider = new firebase.auth.GoogleAuthProvider();
-  const auth = firebase.auth()
-  export const GoogleLogin = () => {
-    
-    auth.signInWithPopup(provider).then(function (result) {
+export const GoogleLogin = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const token = result.credential.accessToken;
         console.log("Token =>", token)
@@ -45,4 +30,4 @@ const  firebaseConfig = {
     });
 }
 
-  //export default firebase;
+//export default GoogleLogin;

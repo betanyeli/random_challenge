63a4fc5140daf11ea27e241640f39d484878b008
betanyeli <betangelii@gmail.com>
2020-05-28 01:41:50 -0400

    fix show info

--- src/screens/ResultApi.js
+++ src/screens/ResultApi.js
@@ -1,27 +1,43 @@
-import React from 'react';
-
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import {
+  Grid,
+  Card,
+  CardMedia,
+  CardHeader,
+} from "@material-ui/core";
 
 export default class ResultApi extends React.Component {
   state = {
-    persons: []
-  }
+    persons: [],
+  };
 
   componentDidMount() {
-    axios.get(`https://randomuser.me/api/?results=20`)
-      .then(res => {
-        const persons = res.data.results;
-        console.log("res", res.data.results)
-        this.setState({ persons });
-      })
+    axios.get(`https://randomuser.me/api/?results=20`).then((res) => {
+      const persons = res.data.results;
+      console.log("res", res.data.results);
+      this.setState({ persons });
+    });
   }
 
   render() {
-      console.log(this.state.persons)
     return (
-      <ul>
-        {this.state.persons.map(el=> <li>{el.name.title}</li>)}
-      </ul>
-    )
+      <Grid container>
+        <Grid item xs={12} sm={6} md={6}>
+          {this.state.persons.map((el, id) => 
+            <Card key={id}>
+              {el.name.title}
+              <CardHeader>
+              
+                <CardMedia
+                        imageUrl={el.picture.thumbnail}
+                        title="Card Image" />
+            
+              </CardHeader>
+            </Card>
+          )}
+        </Grid>
+      </Grid>
+    );
   }
 }
